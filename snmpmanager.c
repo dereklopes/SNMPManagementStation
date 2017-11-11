bfbe77ed50ccb1bfb87b0981e2d8da47285037d0
Derek Lopes <derkle@live.com>
2017-11-11 03:07:31 -0800

    Created snmp_walk function

--- snmpmanager.c
+++ snmpmanager.c
@@ -6,6 +6,8 @@
 
 #define DEBUG false
 
+netsnmp_pdu *snmp_walk(netsnmp_session* open_session, char* oid);
+
 int main(int argc, char **argv) {
     // Get input for sample time interval, # of samples, agent ip, community name
     int sample_interval, num_samples;
@@ -24,7 +26,6 @@ int main(int argc, char **argv) {
         scanf("%s", community);
     };
 
-
     netsnmp_session session, *ss;
     netsnmp_pdu *pdu;
     netsnmp_pdu *response;
@@ -36,29 +37,21 @@ int main(int argc, char **argv) {
     int status;
     int count = 1;
 
-    /*
-     * Initialize the SNMP library
-     */
+    // Initialize the SNMP library
     init_snmp("snmpmanager");
 
-    /*
-     * Initialize a "session" that defines who we're going to talk to
-     */
+    // Initialize a "session" that defines who we're going to talk to
     snmp_sess_init(&session);                   /* set up defaults */
     session.peername = strdup(agent_ip);
 
-    /* set up the authentication parameters for talking to the server */
-
-    /* set the SNMP version number */
+    // set the SNMP version number
     session.version = SNMP_VERSION_1;
 
-    /* set the SNMPv1 community name used for authentication */
+    // set the SNMPv1 community name used for authentication
     session.community = community;
     session.community_len = strlen(session.community);
 
-    /*
-     * Open the session
-     */
+    // Open the session
     SOCK_STARTUP;
     ss = snmp_open(&session);
 
@@ -68,10 +61,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    /*
-     * Create the PDU for the data for our request.
-     *   1) We're going to GET the system.sysDescr.0 node.
-     */
+    // Create the PDU for the data for our request.
     pdu = snmp_pdu_create(SNMP_MSG_GET);
     anOID_len = MAX_OID_LEN;
     if (!snmp_parse_oid(".1.3.6.1.2.1.2.1.0", anOID, &anOID_len)) {
@@ -114,4 +104,36 @@ int main(int argc, char **argv) {
 
     SOCK_CLEANUP;
     return (0);
-} /* main() */
+}
+
+netsnmp_pdu *snmp_walk(netsnmp_session* open_session, char* oid) {
+    // Create the pdu for the request
+    netsnmp_pdu* pdu = snmp_pdu_create(SNMP_MSG_GET), *response;
+    oid anOID[MAX_OID_LEN];
+    size_t anOID_len = MAX_OID_LEN;
+    if (!snmp_parse_oid(oid, anOID, &anOID_len)) {
+        snmp_perror(oid);
+        SOCK_CLEANUP;
+        exit(1);
+    }
+
+    snmp_add_null_var(pdu, anOID, anOID_len);
+
+    // Send the Request out.
+    int status = netsnmp_query_walk(pdu->variables, open_session);
+
+    // Process the response
+    if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
+        // SUCCESS: Return the result pdu
+        return response;
+    } else {
+        // FAILURE: Print what went wrong
+        if (status == STAT_SUCCESS)
+            fprintf(stderr, "Error in packet\nReason: %s\n", snmp_errstring(response->errstat));
+        else if (status == STAT_TIMEOUT)
+            fprintf(stderr, "Timeout: No response from %s.\n", open_session->peername);
+        else
+            snmp_sess_perror("snmpmanager", open_session);
+        return null;
+    }
+}
