ca6216bb0e25ef1ea1fff7cdc47231d9c0385c98
Derek Lopes <derkle@live.com>
2017-11-11 02:43:16 -0800

    Modified to report number of network interfaces

--- snmpmanager.c
+++ snmpmanager.c
@@ -2,140 +2,116 @@
 #include <net-snmp/net-snmp-includes.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, char ** argv)
-{
-  // Get input for sample time interval, # of samples, agent ip, community name
-  int sample_interval, num_samples;
-  char agent_ip[15], community[50];
+#define DEBUG false
 
-  printf("Enter a time interval: ");
-  scanf("%d", &sample_interval);
-  printf("Enter a number of samples: ");
-  scanf("%d", &num_samples);
-  printf("Enter the SNMP agent's IP: ");
-  scanf("%s", agent_ip);
-  printf("Enter the community name: ");
-  scanf("%s", community);
+int main(int argc, char **argv) {
+    // Get input for sample time interval, # of samples, agent ip, community name
+    int sample_interval, num_samples;
+    char *agent_ip, *community;
+    if (DEBUG) {
+        agent_ip = "127.0.0.1";
+        community = "local";
+    } else {
+        printf("Enter a time interval: ");
+        scanf("%d", &sample_interval);
+        printf("Enter a number of samples: ");
+        scanf("%d", &num_samples);
+        printf("Enter the SNMP agent's IP: ");
+        scanf("%s", agent_ip);
+        printf("Enter the community name: ");
+        scanf("%s", community);
+    };
 
-  netsnmp_session session, *ss;
-  netsnmp_pdu *pdu;
-  netsnmp_pdu *response;
 
-  oid anOID[MAX_OID_LEN];
-  size_t anOID_len;
+    netsnmp_session session, *ss;
+    netsnmp_pdu *pdu;
+    netsnmp_pdu *response;
 
-  netsnmp_variable_list *vars;
-  int status;
-  int count=1;
+    oid anOID[MAX_OID_LEN];
+    size_t anOID_len;
 
-  /*
-   * Initialize the SNMP library
-   */
-  init_snmp("snmpmanager");
+    netsnmp_variable_list *vars;
+    int status;
+    int count = 1;
 
-  /*
-   * Initialize a "session" that defines who we're going to talk to
-   */
-  snmp_sess_init( &session );                   /* set up defaults */
-  session.peername = strdup(agent_ip);
-
-  /* set up the authentication parameters for talking to the server */
-
-  /* set the SNMP version number */
-  session.version = SNMP_VERSION_1;
-
-  /* set the SNMPv1 community name used for authentication */
-  session.community = community;
-  session.community_len = strlen(session.community);
-
-  /*
-   * Open the session
-   */
-  SOCK_STARTUP;
-  ss = snmp_open(&session);
-
-  if (!ss) {
-    snmp_sess_perror("ack", &session);
-    SOCK_CLEANUP;
-    exit(1);
-  }
-  
-  /*
-   * Create the PDU for the data for our request.
-   *   1) We're going to GET the system.sysDescr.0 node.
-   */
-  pdu = snmp_pdu_create(SNMP_MSG_GET);
-  anOID_len = MAX_OID_LEN;
-  if (!snmp_parse_oid(".1.3.6.1.2.1.1.1.0", anOID, &anOID_len)) {
-    snmp_perror(".1.3.6.1.2.1.1.1.0");
-    SOCK_CLEANUP;
-    exit(1);
-  }
-#if OTHER_METHODS
-  /*
-   *  These are alternatives to the 'snmp_parse_oid' call above,
-   *    e.g. specifying the OID by name rather than numerically.
-   */
-  read_objid(".1.3.6.1.2.1.1.1.0", anOID, &anOID_len);
-  get_node("sysDescr.0", anOID, &anOID_len);
-  read_objid("system.sysDescr.0", anOID, &anOID_len);
-#endif
-
-  snmp_add_null_var(pdu, anOID, anOID_len);
-
-  /*
-   * Send the Request out.
-   */
-  status = snmp_synch_response(ss, pdu, &response);
-
-  /*
-   * Process the response.
-   */
-  if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
     /*
-     * SUCCESS: Print the result variables
+     * Initialize the SNMP library
      */
+    init_snmp("snmpmanager");
 
-    for(vars = response->variables; vars; vars = vars->next_variable)
-      print_variable(vars->name, vars->name_length, vars);
+    /*
+     * Initialize a "session" that defines who we're going to talk to
+     */
+    snmp_sess_init(&session);                   /* set up defaults */
+    session.peername = strdup(agent_ip);
 
-    /* manipuate the information ourselves */
-    for(vars = response->variables; vars; vars = vars->next_variable) {
-      if (vars->type == ASN_OCTET_STR) {
-        char *sp = (char *)malloc(1 + vars->val_len);
-        memcpy(sp, vars->val.string, vars->val_len);
-        sp[vars->val_len] = '\0';
-        printf("value #%d is a string: %s\n", count++, sp);
-        free(sp);
-      }
-      else
-        printf("value #%d is NOT a string! Ack!\n", count++);
+    /* set up the authentication parameters for talking to the server */
+
+    /* set the SNMP version number */
+    session.version = SNMP_VERSION_1;
+
+    /* set the SNMPv1 community name used for authentication */
+    session.community = community;
+    session.community_len = strlen(session.community);
+
+    /*
+     * Open the session
+     */
+    SOCK_STARTUP;
+    ss = snmp_open(&session);
+
+    if (!ss) {
+        snmp_sess_perror("ack", &session);
+        SOCK_CLEANUP;
+        exit(1);
     }
-  } else {
+
     /*
-     * FAILURE: print what went wrong!
+     * Create the PDU for the data for our request.
+     *   1) We're going to GET the system.sysDescr.0 node.
      */
+    pdu = snmp_pdu_create(SNMP_MSG_GET);
+    anOID_len = MAX_OID_LEN;
+    if (!snmp_parse_oid(".1.3.6.1.2.1.2.1.0", anOID, &anOID_len)) {
+        snmp_perror(".1.3.6.1.2.1.1.1.0");
+        SOCK_CLEANUP;
+        exit(1);
+    }
 
-    if (status == STAT_SUCCESS)
-      fprintf(stderr, "Error in packet\nReason: %s\n",
-              snmp_errstring(response->errstat));
-    else if (status == STAT_TIMEOUT)
-      fprintf(stderr, "Timeout: No response from %s.\n",
-              session.peername);
-    else
-      snmp_sess_perror("snmpdemoapp", ss);
-  }
+    snmp_add_null_var(pdu, anOID, anOID_len);
 
-  /*
-   * Clean up:
-   *  1) free the response.
-   *  2) close the session.
-   */
-  if (response)
-    snmp_free_pdu(response);
-  snmp_close(ss);
+    // Send the Request out.
+    status = snmp_synch_response(ss, pdu, &response);
 
-  SOCK_CLEANUP;
-  return (0);
+    // Process the response
+    if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
+        // SUCCESS: Print the result variable
+        netsnmp_vardata num_of_if;
+        for (vars = response->variables; vars; vars = vars->next_variable) {
+            num_of_if = vars->val;
+        }
+        printf("Number of interfaces: %ld\n", *(num_of_if.integer));
+    } else {
+        // FAILURE: Print what went wrong
+        if (status == STAT_SUCCESS)
+            fprintf(stderr, "Error in packet\nReason: %s\n", snmp_errstring(response->errstat));
+        else if (status == STAT_TIMEOUT)
+            fprintf(stderr, "Timeout: No response from %s.\n", session.peername);
+        else
+            snmp_sess_perror("snmpmanager", ss);
+    }
+
+    /*
+     * Clean up:
+     *  1) free the response.
+     *  2) close the session.
+     */
+    if (response)
+        snmp_free_pdu(response);
+    snmp_close(ss);
+
+    SOCK_CLEANUP;
+    return (0);
 } /* main() */
